f37cd65c80845186c7efd0326475b49ce3092144
tamura <tamura2004@gmail.com>
2019-06-23 16:37:44 +0900

    Add queue

--- sketch.js
+++ sketch.js
@@ -1,189 +1,319 @@
 const Vector = p5.Vector;
+const WIDTH = 80;
+const HEIGHT = 60;
+const PC = [];
+const WEB = [];
+let NODES = [];
+let LB = null;
+let DB = null;
+const colors = ['#c93a40', '#0074bf', '#56a764', '#de9610', '#65ace4', '#f2cf01', '#d16b16'];
 
-class Edge {
-  constructor(v, node) {
-    this.v = v;
-    this.node = node;
-    this.tasks = [];
+class Queue {
+  constructor() {
+    this.right = [];
+    this.left = [];
   }
 
+  get both() {
+    return this.right.concat(this.left);
+  }
+}
+
+class Nodes {
+  constructor() {
+    this.right = null;
+    this.left = null;
+  }
+
+  get shape() {
+    return this.right ? (this.left ? 'BOTH' : 'RIGHT') : (this.left ? 'LEFT' : 'NONE');
+  }
+}
+
+class Edges {
+  constructor() {
+    this.right = [];
+    this.left = [];
+  }
+
+  get shape() {
+    return this.right.length > 0 ? (this.left.length > 0 ? 'BOTH' : 'RIGHT') : (this.left.length > 0 ? 'LEFT' : 'NONE');
+  }
+
+  get both() {
+    return this.right.concat(this.left);
+  }
+}
+
+class Edge {
+  constructor(speed = 6) {
+    this.speed = speed;
+    this.tasks = new Queue();
+    this.nodes = new Nodes();
+  }
+  
   push(task) {
-    task.v = this.v;
-    task.dest = this.node.pos;
+    if (task.direction !== 'RIGHT' && task.direction !== 'LEFT') {
+      throw new Error(`Task direction must be right or left, but it is actually ${task.direction}`);
+    }
+
+    if (this.shape === 'NONE') {
+      throw new Error('Cannot add a task to edge that have no node');
+
+    } else if (this.nodes.shape === 'LEFT' || (this.nodes.shape === 'BOTH' && task.direction === 'LEFT')) {
+      task.destination = this.nodes.left.position;
+      task.speed = this.speed;
+      this.tasks.left.push(task);
+      
+    } else {
+      task.destination = this.nodes.right.position;
+      task.speed = this.speed;
+      this.tasks.right.push(task);
+    }
   }
 
   next() {
-    for (const task of this.tasks) {
+    const nextRightQueue = [];
+    for (const task of this.tasks.right) {
+      task.next();
       if (task.arrive) {
-        this.node.push(this.tasks.shift());
+        this.nodes.right.push(task);
+      } else {
+        nextRightQueue.push(task);
       }
+    }
+    this.tasks.right = nextRightQueue;
+
+    const nextLeftQueue = [];
+    for (const task of this.tasks.left) {
+      task.next();
+      if (task.arrive) {
+        this.nodes.left.push(task);
+      } else {
+        nextLeftQueue.push(task);
+      }
+    }
+    this.tasks.left = nextLeftQueue;
+  }
+
+  draw() {
+    if (this.nodes.shape === 'BOTH') {
+      fill(0);
+      line(this.nodes.left.position.x, this.nodes.left.position.y, this.nodes.right.position.x, this.nodes.right.position.y);
     }
   }
 }
 
 class Node {
-  constructor(pos, cpu, work, dir, edge) {
-    this.pos = pos; // p5.Vector .. position
-    this.cpu = cpu; // CPU power produced by this node
-    this.work = work; // CPU power required for each task
-    this.dir = dir; // 1 for upstream, -1 for downstream
-    this.edge = edge;
-    this.tasks = [];
+  constructor(name, color, position, cpu, power) {
+    this.name = name;
+    this.color = color;
+    this.position = position;
+    this.cpu = cpu;
+    this.power = power; // CPU power consumed by a task to exit
+    this.tasks = new Queue();
+    this.edges = new Edges();
+  }
+
+  link(nodes, direction) {
+    for (const node of nodes) {
+      const edge = new Edge();
+      if (direction === 'RIGHT') {
+        if (node.name === 'LB') {
+          edge.speed = 4;
+        }
+        edge.nodes.left = this;
+        edge.nodes.right = node;
+        this.edges.right.push(edge);
+      } else if (direction === 'LEFT') {
+        if (this.name === 'LB') {
+          edge.speed = 4;
+        }
+        edge.nodes.right = this;
+        edge.nodes.left = node;
+        this.edges.left.push(edge);
+      }
+    }
   }
 
   push(task) {
-    task.wait = this.work;
-    task.v = 2;
-    task.dest = new Vector(this.pos.x - 30 * this.tasks.length * this.dir, this.pos.y);
-    this.tasks.push(task);
+    task.power = this.power;
+    task.speed = 2;
+    task.position = this.position.copy();
+    if (task.direction === 'RIGHT') {
+      task.destination = this.position.copy();
+      this.tasks.right.push(task);
+    } else if (task.direction === 'LEFT') {
+      task.destination = this.position.copy();
+      this.tasks.left.push(task);
+    } else {
+      throw new Error(`Bad task direction: ${task.direction}`);
+    }
   }
 
   next() {
-    let pool = this.cpu;
-    while (pool > 0 && this.tasks.length > 0) {
-      const task = this.tasks[0];
-      if (task.wait > pool) {
-        task.wait -= pool;
-        pool = 0;
-      } else {
-        pool -= task.wait;
-        task.wait = 0;
-        this.tasks.shift();
-        if (this.edge !== undefined) {
-          this.edge.push(task);
+    let work = this.cpu;
+    while (work > 0 && (this.tasks.right.length > 0 || this.tasks.left.length > 0)) {
+      if (this.tasks.right.length > 0) {
+        if (this.tasks.right[0].power <= work) {
+          const task = this.tasks.right.shift();
+          work -= task.power;
+          task.power = 0;
+          if (this.edges.shape === 'BOTH' || this.edges.shape === 'RIGHT') {
+            random(this.edges.right).push(task);
+          } else if (this.edges.shape === 'LEFT' ) {
+            task.direction = 'LEFT';
+            random(this.edges.left).push(task);
+            // this.edges.left[0].push(task);
+          } else {
+            throw new Error(`Node ${this.name} has no edge`);
+          }
+        } else {
+          this.tasks.right[0].power -= work;
+          work = 0;
+        }
+      }
+      if (this.tasks.left.length > 0) {
+        if (this.tasks.left[0].power <= work) {
+          const task = this.tasks.left.shift();
+          work -= task.power;
+          task.power = 0;
+          if (this.edges.shape === 'BOTH' || this.edges.shape === 'LEFT') {
+            const i = colors.indexOf(task.color);
+            const edge = (i !== -1 && this.name === 'LB') ? this.edges.left[i] : random(this.edges.left);
+            edge.push(task);
+          } else if (this.edges.shape === 'RIGHT') {
+            // I'm home.
+          } else {
+            throw new Error('Node has no edge');
+          }
+        } else {
+          this.tasks.left[0].power -= work;
+          work = 0;
         }
       }
     }
-    for (let i = 0; i < this.tasks.length; i++) {
-      task.dest = new Vector(this.pos.x - 30 * i, this.pos.y);
+    for (let i = 0; i < this.tasks.right.length; i++) {
+      this.tasks.right[i].destination = new Vector(this.position.x + 20, this.position.y + 20 * i);
     }
-  }
-}
-
-class Ball {
-  constructor(color) {
-    this.dest = dest; // destination position p5.Vector
-    this.p = path.shift()[0].copy();
-    const q = path.shift();
-    this.q = q[0];
-    this.wait = q[1];
-    this.path = path;
-    this.visible = true;
-    this.color = color;
-  }
-
-  get dist() {
-    return p5.Vector.dist(this.p, this.q);
-  }
-
-  get v() {
-    const l = this.path.length;
-    // const speed = [5, 3, 2, 0].includes(l) ? 4 : 8;
-    const speed = 4;
-    return p5.Vector.sub(this.q, this.p).normalize().mult(speed);
-  }
-
-  move() {
-    if (this.dist < 10) {
-      this.park();
-    } else {
-      this.p = this.p.add(this.v);
+    for (let i = 0; i < this.tasks.left.length; i++) {
+      this.tasks.left[i].destination = new Vector(this.position.x - 20, this.position.y - 20 * i);
     }
-  }
-
-  park() {
-    this.wait--;
-    if (this.wait === 0) {
-      this.nextDestination();
-      // this.wait = 20;
+    for (const edge of this.edges.both) {
+      edge.next();
     }
-  }
-
-  nextDestination() {
-    if (this.path.length > 0) {
-      const q = this.path.shift();
-      this.q = q[0];
-      this.wait = q[1];
-    } else {
-      this.visible = false;
+    for (const task of this.tasks.both) {
+      task.next();
     }
   }
 
   draw() {
     fill(this.color);
-    ellipse(this.p.x, this.p.y, 15);
-    // text(`${this.path}`, this.p.x, this.p.y)
+    rect(this.position.x - WIDTH / 2, this.position.y - HEIGHT / 2, WIDTH, HEIGHT);
+    if (this.color === 'white') {
+      fill('black');
+    } else {
+      fill('white');
+    }
+    text(this.name, this.position.x - WIDTH * 0.4, this.position.y - HEIGHT * 0.2);
+    text(`${this.tasks.both.length}`, this.position.x - WIDTH * 0.4, this.position.y);
+  }
+
+  createTask() {
+    this.push(new Task(this.color));
   }
 }
 
-let balls = [];
+class Task {
+  constructor(color = 128, direction = 'RIGHT') {
+    this.color = color;
+    this.direction = direction;
+    this.position = null;
+    this.destination = null;
+    this.speed = null;
+    this.power = null; // Consumed CPU power to exit from node
+    this.size = 15;
+  }
 
+  get distance() {
+    if (this.destination === null || this.position === null) {
+      throw new Error('Bad task property');
+    }
+    return p5.Vector.dist(this.position, this.destination);
+  }
+
+  get arrive() {
+    return this.distance < this.speed / 2;
+  }
+
+  next() {
+    if (this.position === null || this.destination === null || this.speed === null) {
+      throw new Error('Bad task property');
+    }
+    const v = p5.Vector.sub(this.destination, this.position).normalize().mult(this.speed);
+    this.position.add(v);
+  }
+
+  draw() {
+    fill(this.color);
+    ellipse(this.position.x, this.position.y, this.size);
+    fill('black');
+    // text(String(this), this.position.x, this.position.y);
+  }
+}
 
 function setup() {
   createCanvas(1280, 1024);
+  for (let i = 1; i < 6; i++) {
+    const position = new Vector(100, 100 * i);
+    PC.push(new Node(`PC#${i}`, colors[i - 1], position, 10, 0));
+  }
+  LB = new Node('LB', 'white', new Vector(400, 300), 10, 100);
+  for (let i = 1; i < 4; i++) {
+    const position = new Vector(700, i * 200 - 100);
+    WEB.push(new Node(`WEB#${i}`, 'white', position, 10, 300));
+  }
+  DB = new Node('DB', 'white', new Vector(1000, 300), 10, 200)
+
+  for (const pc of PC) {
+    pc.link([LB], 'RIGHT');
+  }
+  LB.link(WEB, 'RIGHT');
+  LB.link(PC, 'LEFT');
+  for (const web of WEB) {
+    web.link([DB], 'RIGHT');
+    web.link([LB], 'LEFT');
+  }
+  DB.link(WEB, 'LEFT');
+  NODES = [...PC, LB, ...WEB, DB];
 }
 
 function draw() {
   background(255);
-  const WIDTH = 80;
-  const HEIGHT = 60;
 
-  const PC = [1,2,3,4,5].map((i) => createVector(100, i * 100 ));
-  const LB = createVector(400, 300);
-  const WEB = [1,2,3].map((i) => createVector(700, i * 200 - 100));
-  const DB = createVector(1000, 300);
-  const NODES = [...PC, LB, ...WEB, DB];
-  const colors = ['#c93a40', '#0074bf', '#56a764', '#de9610', '#65ace4', '#f2cf01', '#d16b16'];
-
-  // network
-  for (let i = 0; i < 5; i++) {
-    fill(0);
-    line(PC[i].x, PC[i].y, LB.x, LB.y);
-    fill(colors[i]);
-    rect(PC[i].x - WIDTH / 2, PC[i].y - HEIGHT / 2, WIDTH, HEIGHT);
-    fill(255);
-    text(`PC#${i+1}`, PC[i].x - WIDTH * 0.4, PC[i].y);
-  }
-  for (let i = 0; i < 3; i++) {
-    fill(0);
-    line(LB.x, LB.y, WEB[i].x, WEB[i].y);
-    line(DB.x, DB.y, WEB[i].x, WEB[i].y);
-    fill(255);
-    rect(WEB[i].x - WIDTH / 2, WEB[i].y - HEIGHT / 2, WIDTH, HEIGHT);
-    fill(0);
-    text(`WEB#${i+1}`, WEB[i].x - WIDTH * 0.4, WEB[i].y);
+  if (random() < 0.06) {
+    random(PC).createTask();
   }
 
-  fill(255);
-  rect(LB.x - WIDTH / 2, LB.y - HEIGHT / 2, WIDTH, HEIGHT);
-  rect(DB.x - WIDTH / 2, DB.y - HEIGHT / 2, WIDTH, HEIGHT);
-  fill(0);
-  textSize(18);
-  text('LB', LB.x - WIDTH * 0.4, LB.y);
-  text('DB', DB.x - WIDTH * 0.4, DB.y);
-
-  const pcIndex = random([0,1,2,3,4]);
-  const path = [
-    [ PC[pcIndex], 1 ],
-    [ LB, 1 ],
-    [ random(WEB), 100 ],
-    [ DB, 300 ],
-    [ random(WEB), 100 ],
-    [ LB, 1 ],
-    [ PC[pcIndex], 1 ],
-  ];
-  const rv = p5.Vector.random2D().mult(20);
-  if (random() < 0.1) {
-    balls.push(new Ball(
-      path.map((p) => [p[0].copy().add(rv), p[1]]),
-      color(colors[pcIndex]),
-    ));
+  for (const node of NODES) {
+    for (const edge of node.edges.both) {
+      edge.draw();
+    }
   }
-
-  for (const ball of balls) {
-    ball.move();
-    ball.draw();
+  for (const node of NODES) {
+    node.draw();
+    node.next();
   }
-
-  balls = balls.filter((ball) => ball.visible);
+  for (const node of NODES) {
+    for (const edge of node.edges.both) {
+      for (const task of edge.tasks.both) {
+        task.draw();
+      }
+    }
+  }
+  for (const node of NODES) {
+    for (const task of node.tasks.both) {
+      task.draw();
+    }
+  }
 }
